79e9ce47a910032f830473f1ded43f85bc3b8889
Damian Delgado <ddelgado@codigodelsur.com>
2016-01-04 12:32:30 -0200

    Added example to setRoutes method

--- core/lib/server/router.js
+++ core/lib/server/router.js
@@ -2,6 +2,33 @@ var express = require('express');
 var router = express.Router();
 var Authenticator = require("../Authenticator");
 
+
+/*** Centralized method to add a route to the router and set its middleware***/
+
+/*
+
+Example :
+
+If we call setRoutes("company",
+					[
+						{ method : "get" , path : "/" , action : CompanyController.get , role : "client" },
+						{ method : "get" , path : "/:id" , action : CompanyController.getByUserId , role : "client" },
+						{ method : "post" , path : "/:id" , action : CompanyController.createCompany , role : "admin" }
+					])
+
+we are doing this :
+
+router.get("/company/",Autenthicator.ensureAuthenticated('client',req,res,next),CompanyController.get);
+
+router.get("/company/:id",Autenthicator.ensureAuthenticated('client',req,res,next),CompanyController.getByUserId);
+
+router.post("/company/",Autenthicator.ensureAuthenticated('admin',req,res,next),CompanyController.createCompany);
+
+*/
+
+
+
+
 var setRoutes = function(model,routes){
 	for(var i in routes){
 		var route = routes[i];
@@ -10,11 +37,6 @@ var setRoutes = function(model,routes){
 }
 
 
-// middleware that is specific to this router
-// router.use(function timeLog(req, res, next) {
-//   console.log('Time: ', Date.now());
-//   next();
-// });
 
 var roleBasedMiddleware = {
 	admin : function(req,res,next){
